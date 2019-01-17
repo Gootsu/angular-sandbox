ba2c422f43234d440f4706d0c2912b618ea0af5b
Gootsu <fmatasalvarez@gmail.com>
2019-01-17 17:31:23 +0100

    Dependency Vulnerability fix, SASS and added missing bootstrap dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,11 @@
+
+/**
+ * @license
+ * Copyright Gootsu. All Rights Reserved.
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/Gootsu/angular-sandbox/blob/master/LICENSE 
+ */
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
